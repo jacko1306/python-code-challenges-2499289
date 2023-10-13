37c4d0c9995072de1da1ffa3aa4f63c191b8449b
jacko1306 <45062242+jacko1306@users.noreply.github.com>
2023-10-13 07:31:14 +0000

    no string

--- 01_01/01_01.py
+++ 01_01/01_01.py
@@ -1,8 +1,10 @@
 print("Bitte geben Sie eine Zahl zwischen 0 und 9 ein: ")
 wert = input()
 
-if (len(wert) > 1):
+if (len(wert) > 1 ):
     print("too big")
+elif (wert == ""):
+    print("not in 0-9")
 else:
     print("yes")
     
